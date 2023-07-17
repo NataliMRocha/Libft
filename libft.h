38effa94281bfb6c9b8e8803a08d141468814996
Natali Moreira Rocha <namoreir@c2r2p1.42sp.org.br>
2023-07-17 20:09:03 -0300

    lib

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 16:11:17 by namoreir          #+#    #+#             */
+/*   Updated: 2023/07/17 20:07:14 by namoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# define TRUE 1
+# define FALSE 0
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	strlen(const char *s);
+
+#endif
