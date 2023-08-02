ab456c2da55ce1454fe1e0e8d5d344fc133fce58
Natali Moreira Rocha <namoreir@c3r4p5.42sp.org.br>
2023-08-02 17:32:32 -0300

    bonus

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 13:19:01 by namoreir          #+#    #+#             */
+/*   Updated: 2023/08/02 17:26:12 by namoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)ft_calloc(1, sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content; 
+	new->next = NULL;
+	return (new);
+}
