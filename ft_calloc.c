17997df7fc77e93df3b1e87af59cc2183a3f9804
Natali Moreira Rocha <namoreir@c3r5p5.42sp.org.br>
2023-08-08 20:12:00 -0300

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 19:29:15 by namoreir          #+#    #+#             */
-/*   Updated: 2023/07/28 16:18:31 by namoreir         ###   ########.fr       */
+/*   Updated: 2023/08/08 20:07:16 by namoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	total_size;
 
 	total_size = nmemb * size;
-	if ((nmemb == 0 || size == 0) || total_size / size != nmemb)
+	if (total_size == 0)
 		return (malloc(0));
+	if (total_size > __INT_MAX__ || total_size / size != nmemb)
+		return (NULL);
 	alloc_mem = malloc(total_size);
 	if (!alloc_mem)
 		return (NULL);
