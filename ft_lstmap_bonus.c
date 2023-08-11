70fdebfef571864e61719bfd60db2c086f480a52
Natali Moreira Rocha <namoreir@c1r3p3.42sp.org.br>
2023-08-11 17:25:25 -0300

    ft_lstmap_bonus

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 12:15:42 by namoreir          #+#    #+#             */
-/*   Updated: 2023/08/08 19:46:00 by namoreir         ###   ########.fr       */
+/*   Updated: 2023/08/10 11:42:03 by namoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*new_node;
 
+	if (lst == NULL || f == NULL)
+		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
 		if (new_node == NULL)
 		{
+			if (new_lst == NULL)
+				return (NULL);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
